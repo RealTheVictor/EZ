45b359b1572b9e3a119920f564c8ec786bfd1e9a
RealTheVictor <81240253+RealTheVictor@users.noreply.github.com>
2022-06-28 21:40:34 +0300

    Create coolkids.lua

---
+++ coolkids.lua
@@ -0,0 +1,34 @@
+while true do
+	local chr = game.Players.wTheVictor
+  local chr2 = game.Players.davidgbr34
+  local chr3 = game.Players.alexfuratot1
+  local chr4 = game.Players.miladumi
+    wait()
+    chr.Character.Humanoid.DisplayName = "[😀]" .. chr4.DisplayName
+    for i, v in pairs(chr4.Character.Head:GetChildren)
+      if v:IsA("Decal") then
+        v.Texture = "rbxassetid:42070872"
+    chr.Character.Humanoid.DisplayName = "[👑]" .. chr.DisplayName
+    chr.Character.Head.MeshId = 1
+    chr.Character.RightLowerLeg.MeshId = "902942093"
+	  chr.Character.RightLowerLeg.Transparency = "1"
+	  chr.Character.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
+	  chr.Character.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
+	  chr.Character.RightFoot.MeshId = "902942089"
+	  chr.Character.RightFoot.Transparency = "1"
+    chr3.Character.Humanoid.DisplayName = "[💀]" .. chr3.DisplayName
+    chr3.Character.Head.MeshId = 1
+    chr2.Character.Humanoid.DisplayName = "[🐴]" .. chr2.DisplayName
+    chr2.Character.Head.MeshId = 1
+    chr2.Character.RightLowerLeg.MeshId = "902942093"
+	  chr2.Character.RightLowerLeg.Transparency = "1"
+	  chr2.Character.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
+	  chr2.Character.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
+	  chr2.Character.RightFoot.MeshId = "902942089"
+	  chr2.Character.RightFoot.Transparency = "1"
+--    for i, v in pairs(chr.Character:GetChildren()) do
+--        if v:IsA("Accessory") then
+--            v:Destroy()
+--      end
+--    end
+--  end
