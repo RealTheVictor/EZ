4a95f484c444af88692d5ef4c709099c0f617f6b
RealTheVictor <81240253+RealTheVictor@users.noreply.github.com>
2022-07-03 13:24:38 +0300

    Update nohats.lua

--- nohats.lua
+++ nohats.lua
@@ -1,8 +1,9 @@
 local playerslol = game:GetService("Players")
 local chr = playerslol:FindFirstChild("wTheVictor")
 
-for i, v in pairs(chr.Character:GetChildren()) do
+--[[for i, v in pairs(chr.Character:GetChildren()) do
   if v:IsA("Accessory") then
 		v:Destroy()
     end
   end
+]]--
