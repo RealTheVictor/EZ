d96a17dd63a74c57e85e0dc3cf32f0c0574e29e9
RealTheVictor <81240253+RealTheVictor@users.noreply.github.com>
2022-10-24 15:49:45 +0300

    Update Emoji.lua

--- Emoji.lua
+++ Emoji.lua
@@ -1,59 +1,57 @@
-local Stars = {
+local Star = {
 	[1075964145] = true, -- hecracksmynuts
-    [2744351621] = true, -- ola
-    [1065383362] = true, -- dam
-    [177021005] = true, -- abytra
-    [2254276157] = true, -- alexia
-    [219780420] = true, -- vlixx
-    [1371293488] = true, -- mara
-    [2395886509] = true, -- per
-    [966431623] = true, -- mister roblox man
-    [2412658776] = true, -- noya
-    [676045957] = true, -- vivefreak
+    	[2744351621] = true, -- ola
+    	[1065383362] = true, -- dam
+    	[177021005] = true, -- abytra
+    	[2254276157] = true, -- alexia
+    	[219780420] = true, -- vlixx
+    	[1371293488] = true, -- mara
+    	[2395886509] = true, -- per
+    	[966431623] = true, -- mister roblox man
+    	[2412658776] = true, -- noya
+    	[676045957] = true, -- vivefreak
 }
 
-local Owners = {
+local Crown = {
 	[1581179193] = true, -- new
 	[300734719] = true, -- old
-    [146656097] = true, -- matra
-    [3368671867] = true, -- trick
+    	[146656097] = true, -- matra
+    	[3368671867] = true, -- trick
 } 
 
 function EZNames()
 	for _,v in pairs(game:GetService('Players'):GetChildren()) do
-		if Stars[v.UserId] then
+		if Star[v.UserId] then
 			if v.Character then
 				if v.Character.Parent.Name == 'Players' then
 					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]'..v.DisplayName)
 				end
 			end
-		elseif Owners[v.UserId] then
+		elseif Crown[v.UserId] then
 			if v.Character then
 				if v.Character.Parent.Name == 'Players' then
 					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑] '..v.DisplayName)
 				end
 			end
 		elseif v.Character then
-			if v.Character.Parent.Name == 'Players' then
-                if not v.Character.LowerTorso:FindFirstChild("WaistFrontAttachment") then
-					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (v.DisplayName.. '(test)')
-                    print("Warning! "..v.DisplayName.."is using Test!")
-				end
-				if not v.Character.UpperTorso:FindFirstChild("BodyFrontAttachment") then
-					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (v.DisplayName.. '(pluto user)')
-                    print("Warning! "..v.DisplayName.."is using Pluto!")
-				end
-				if not v.Character.UpperTorso:FindFirstChild('BodyBackAttachment') then
-					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (v.DisplayName.. '(rayx user)')
-                    print("Warning! "..v.DisplayName.."is using Ray X!")
-				end
-				if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
-					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (v.DisplayName.. '(swagmode user)')
-                    print("Warning! "..v.DisplayName.."is using Swagmode!")
-				end
+                	if not v.Character.LowerTorso:FindFirstChild("WaistFrontAttachment") then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🥵]'..v.DisplayName)
+				print("Warning! "..v.DisplayName.."is using E.Z. S!")
+			end
+			if not v.Character.UpperTorso:FindFirstChild("BodyFrontAttachment") then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (v.DisplayName.. '(pluto user)')
+                		print("Warning! "..v.DisplayName.."is using Pluto!")
+			end
+			if not v.Character.UpperTorso:FindFirstChild('BodyBackAttachment') then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (v.DisplayName.. '(rayx user)')
+                	        print("Warning! "..v.DisplayName.."is using Ray X!")
+			end
+			if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (v.DisplayName.. '(swagmode user)')
+                    		print("Warning! "..v.DisplayName.."is using Swagmode!")
 			end
 		end
 	end
 end
 local success,err = pcall(EZNames)
-return Stars
+return Star
