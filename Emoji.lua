a11762feb4306c408eddc7d3ea61de21b4885727
RealTheVictor <81240253+RealTheVictor@users.noreply.github.com>
2022-12-18 13:47:52 +0200

    Update Emoji.lua

--- Emoji.lua
+++ Emoji.lua
@@ -3,22 +3,22 @@ local Star = {
     	[2744351621] = true, -- ola
 	[3684396760] = true, -- albert
 	[506425146] = true, -- david
-    	[1065383362] = true, -- dam
-    	[177021005] = true, -- abytra
-    	[2254276157] = true, -- alexia
-    	[219780420] = true, -- vlixx
-    	[1371293488] = true, -- mara
-    	[2395886509] = true, -- per
-    	[966431623] = true, -- mister roblox man
-    	[2412658776] = true, -- noya
-    	[676045957] = true, -- vivefreak
+	[1065383362] = true, -- dam
+	[177021005] = true, -- abytra
+	[2254276157] = true, -- alexia
+	[219780420] = true, -- vlixx
+	[1371293488] = true, -- mara
+	[2395886509] = true, -- per
+	[966431623] = true, -- mister roblox man
+	[2412658776] = true, -- noya
+	[676045957] = true, -- vivefreak
 }
 
 local Crown = {
 	[1581179193] = true, -- new
 	[300734719] = true, -- old
-    	[146656097] = true, -- matra
-    	[3368671867] = true, -- trick
+	[146656097] = true, -- matra
+	[3368671867] = true, -- trick
 } 
 
 function EZNames()
@@ -36,22 +36,28 @@ function EZNames()
 				end
 			end
 		elseif v.Character then
-                	if not v.Character.LowerTorso:FindFirstChild("WaistFrontAttachment") then
+            if not v.Character.LowerTorso:FindFirstChild("WaistFrontAttachment") then
 				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🥵]'..v.DisplayName)
-				print("Warning! "..v.DisplayName.."is using E.Z. S!")
+				print("Warning! "..v.DisplayName.." is using E.Z. S!")
 			end
 			if not v.Character.UpperTorso:FindFirstChild("BodyFrontAttachment") then
 				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (v.DisplayName.. '(pluto user)')
-                		print("Warning! "..v.DisplayName.."is using Pluto!")
+                print("Warning! "..v.DisplayName.." is using Pluto!")
 			end
 			if not v.Character.UpperTorso:FindFirstChild('BodyBackAttachment') then
 				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (v.DisplayName.. '(rayx user)')
-                	        print("Warning! "..v.DisplayName.."is using Ray X!")
+                print("Warning! "..v.DisplayName.." is using Ray X!")
 			end
 			if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
-				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (v.DisplayName.. '(swagmode user)')
-                    		print("Warning! "..v.DisplayName.."is using Swagmode!")
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (v.DisplayName.. '(dimag user)')
+                print("Warning! "..v.DisplayName.." is using Dimag X!")
 			end
+			v.Character.DescendantRemoving:Connect(function()
+			    if v.Character then
+			        v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (v.DisplayName.. '(exploiter)')
+			        print("Warning! "..v.DisplayName.." is an exploiter!")
+			    end
+			end)
 		end
 	end
 end
