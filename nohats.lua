a0c6040a3ac5b13f96a0964c24ffb0330a105e29
RealTheVictor <81240253+RealTheVictor@users.noreply.github.com>
2022-12-18 14:16:23 +0200

    Delete nohats.lua

--- nohats.lua
+++
@@ -1,8 +0,0 @@
-local playerslol = game:GetService("Players")
-local chr = playerslol:FindFirstChild("wTheVictor")
-
---for i, v in pairs(chr.Character:GetChildren()) do
---  	if v:IsA("Accessory") then
---		v:Destroy()
---    	end
---end
