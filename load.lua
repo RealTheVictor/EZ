121203acbb08532719e0f745826304b6542404c9
RealTheVictor <81240253+RealTheVictor@users.noreply.github.com>
2022-12-25 17:35:55 +0200

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,5 +1,5 @@
 coroutine.resume(coroutine.create(function()
-    while wait() do
+    while wait(3) do
         local function main()
             loadstring(game:HttpGet('https://raw.githubusercontent.com/RealTheVictor/EZ/main/Emoji.lua'))()
             loadstring(game:HttpGet('https://raw.githubusercontent.com/RealTheVictor/EZ/main/coolkids.lua'))()
